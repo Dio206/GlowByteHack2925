97754951dba80020358ba84b19790212d78ce041
civ <ffff06@yandex.com>
2025-11-24 02:04:19 +0300

    modified:   backend/model/__pycache__/main.cpython-313.pyc 	modified:   backend/model/main.py

--- backend/model/main.py
+++ backend/model/main.py
@@ -152,8 +152,6 @@ model = CatBoostClassifier()
 origins = [
     "http://localhost:5173",
     "http://localhost:8000",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
