50162aa0f97839b964f01b5ad670c1b777db3819
civ <ffff06@yandex.com>
2025-11-24 03:51:52 +0300

    modified:   backend/model/__pycache__/main.cpython-313.pyc 	modified:   backend/model/main.py

--- backend/model/main.py
+++ backend/model/main.py
@@ -153,11 +153,10 @@ app = FastAPI(
 model = CatBoostClassifier()
 
 origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://localhost:5173",
-    "http://localhost:8000",
-    "http://127.0.0.1:5174",
-    "http://127.0.0.1:8000",
-    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
@@ -167,7 +166,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 try:
     if os.path.exists(model_path):
         model.load_model(model_path)
