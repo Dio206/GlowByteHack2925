532dec58487e69c68380a74f9dd12e93b65e6cda
civ <ffff06@yandex.com>
2025-11-24 03:51:30 +0300

    modified:   ReadMe.md 	modified:   backend/model/__pycache__/main.cpython-313.pyc 	modified:   backend/model/main.py 	modified:   backend/model/__pycache__/main.cpython-313.pyc 	modified:   backend/model/main.py

--- backend/model/main.py
+++ backend/model/main.py
@@ -153,7 +153,7 @@ app = FastAPI(
 model = CatBoostClassifier()
 
 origins = [
-    "http://localhost:5174",
+    "http://localhost:5173",
     "http://localhost:8000",
     "http://127.0.0.1:5174",
     "http://127.0.0.1:8000",
